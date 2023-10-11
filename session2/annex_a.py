b54a954c795611ddd478d7b759141d2557b5bc2e
Adam <adam@charming-data.com>
2023-10-11 13:59:22 -0400

    Create annex_a.py

---
+++ session2/annex_a.py
@@ -0,0 +1,45 @@
+# Import packages
+import micropip
+await micropip.install("dash_ag_grid")
+
+from dash import Dash, html, dash_table, dcc, callback, Output, Input
+import pandas as pd
+import plotly.express as px
+import dash_ag_grid as dag
+
+# Incorporate data
+df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/gapminder2007.csv')
+
+# Plotly graphs: https://plotly.com/python/
+fig = px.histogram(df, x='continent', y='pop', histfunc='avg')
+
+# Initialize the app
+app = Dash(__name__)
+
+# App layout
+app.layout = html.Div([
+    html.Div(children='My First App with Data, Graph, and Controls', style={'color': 'yellow'}),
+    html.Hr(),
+    dcc.RadioItems(options=['pop', 'lifeExp', 'gdpPercap'], value='lifeExp', id='column-options', style={'color': 'yellow'}),
+    dag.AgGrid(
+        id="grid",
+        rowData=df.to_dict("records"),
+        columnDefs=[{"field": i} for i in df.columns],
+    ),
+    dcc.Graph(figure=fig, id='graph1')
+])
+
+# Add controls to build the interaction
+@callback(
+    Output(component_id='graph1', component_property='figure'),
+    Input(component_id='column-options', component_property='value')
+)
+def update_graph(col_chosen):
+    fig = px.histogram(df, x='continent', y=col_chosen, histfunc='avg')
+    return fig
+
+
+
+# Run the app
+if __name__ == '__main__':
+    app.run(debug=True)
